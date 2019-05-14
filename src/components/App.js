d39306ca42d0df268ab41f44c4623b026f599446
haney2121 <haney037332@gmail.com>
2019-05-14 13:54:29 -0500

    generatePalette colors

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from '../helpers/seedColors';
 
+import { generatePalette } from '../helpers/colorHelpers';
+
 const App = () => {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div>
       <Palette {...seedColors[1]} />
