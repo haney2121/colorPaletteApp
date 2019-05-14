0e05734404c494fcdfd9f5bb66afe7b2060d3155
haney2121 <haney037332@gmail.com>
2019-05-14 16:37:45 -0500

    fixed palette routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Palette from './Palette';
 import seedColors from '../helpers/seedColors';
@@ -6,10 +7,18 @@ import seedColors from '../helpers/seedColors';
 import { generatePalette } from '../helpers/colorHelpers';
 
 const App = () => {
+
+  const findPalette = id => {
+    return seedColors.find((palette) => {
+      return palette.id === id;
+    })
+  }
+
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette List goes here</h1>} />
+      <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />} />
+    </Switch>
   );
 }
 
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -8,11 +8,13 @@ const Palette = (props) => {
   const [level, setLevel] = useState(500);
   const [format, setFormat] = useState('hex');
 
+  const { paletteName, emoji, colors } = props.palette;
+
   const changeFormat = (val) => {
     setFormat(val);
   }
 
-  const colorBoxes = props.palette.colors[level].map((color) => (
+  const colorBoxes = colors[level].map((color) => (
     <ColorBox key={color.name} background={color[format]} name={color.name} />
   ))
 
@@ -20,14 +22,17 @@ const Palette = (props) => {
     setLevel(level);
   }
 
+
   return (
     <div className="Palette">
       <NavBar changeLevel={changeLevel} level={level} changeFormat={changeFormat} />
-      {/* Navbar here */}
       <div className="Palette-colors">
         {colorBoxes}
       </div>
-      {/* footer here */}
+      <footer className="Palette-footer">
+        {paletteName}
+        <span className="emoji">{emoji}</span>
+      </footer>
     </div>
   )
 }
